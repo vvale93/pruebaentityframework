48e30e6a9b4be1eb57092635851b3c82c5cedcc7
Usuario <Usuario@PC-04>
2016-11-14 20:28:45 -0300

    Segundo Commit prueba

---
+++ ConsoleApplication1/ClassLibrary1/Perfil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Perfil
+    {
+        public int IDPerfil { get; set; }
+        public string Nombre { get; set; }
+
+
+
+    }
+}
---
+++ ConsoleApplication1/ClassLibrary1/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class Usuario
+    {
+        public string Login { get; set; }
+        public DateTime FechaUltimoLogin { get; set; }
+        public int IntentosInvalidos { get; set; }
+        public Perfil Perfil { get; set; }
+
+    }
+}
